f40d83958ee01a80d4934ec725384669f89c4ef2
luke2630 <73398866+luke2630@users.noreply.github.com>
2021-04-19 14:40:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//still figuring this out, although the plan is to eventually have the info written to a google sheet
+
+/*function init() {
+    Tabletop.init( {https://docs.google.com/spreadsheets/d/1LtNlcfAHZjUWQbqp59BLfSiu7zbk-0Yf-Fj7NIhrmb8/edit#gid=0
+    callback: function(data, tabletop) {
+    console.log(data)
+    },
+    simpleSheet: true } )
+    }
+    window.addEventListener(‘DOMContentLoaded’, init)
+*/
+//https://docs.google.com/spreadsheets/d/1LtNlcfAHZjUWQbqp59BLfSiu7zbk-0Yf-Fj7NIhrmb8/edit#gid=0
